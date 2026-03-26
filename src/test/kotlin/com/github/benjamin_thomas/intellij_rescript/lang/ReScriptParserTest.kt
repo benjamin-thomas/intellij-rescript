84b3f3cfce18929d03beed33e788150464899632
Benjamin Thomas <benjamin.guy.thomas@gmail.com>
2026-03-26 23:54:49 +0100

    Add error recovery support to parser, update grammar with recovery rules, and add tests with fixtures

--- src/test/kotlin/com/github/benjamin_thomas/intellij_rescript/lang/ReScriptParserTest.kt
+++ src/test/kotlin/com/github/benjamin_thomas/intellij_rescript/lang/ReScriptParserTest.kt
@@ -2,6 +2,8 @@ package com.github.benjamin_thomas.intellij_rescript.lang
 
 import com.intellij.testFramework.ParsingTestCase
 
+private val noop: () -> Unit = {}
+
 class ReScriptParserTest : ParsingTestCase(
     "com/github/benjamin_thomas/intellij_rescript/parser/fixtures",
     "res",
@@ -10,17 +12,18 @@ class ReScriptParserTest : ParsingTestCase(
 ) {
     override fun getTestDataPath() = System.getProperty("user.dir") + "/src/test/resources"
 
-    private fun runParserTest(inputFile: String, expectedOutputFile: String) =
+    private fun runParserTest(inputFile: String, expectedOutputFile: String, hasParseErrors: Boolean = false) =
         createParserTest(
             createAndSetPsiFile = { file ->
                 val name = file.removeSuffix(".res")
                 createPsiFile(name, loadFile(file)).also { myFile = it }
             },
-            ensureNoErrorElements = ::ensureNoErrorElements,
+            ensureNoErrorElements = if (hasParseErrors) noop else ::ensureNoErrorElements,
             toParseTreeText = { toParseTreeText(it, false, false) },
             fullDataPath = myFullDataPath,
         )(inputFile, expectedOutputFile)
 
     fun testLetBinding() = runParserTest("LetBinding.res", "LetBinding.out")
     fun testMultipleDeclarations() = runParserTest("MultipleDeclarations.res", "MultipleDeclarations.out")
+    fun testErrorRecovery() = runParserTest("ErrorRecovery.res", "ErrorRecovery.out", hasParseErrors = true)
 }
